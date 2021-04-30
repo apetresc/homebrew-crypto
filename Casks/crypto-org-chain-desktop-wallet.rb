d251e57528a83baee52047e980eec0ac57ea61c6
Adrian Petrescu <adrian@apetre.sc>
2021-04-30 17:01:20 -0400

    Crypto.org Chain Desktop Wallet v0.2.1

--- Casks/crypto-org-chain-desktop-wallet.rb
+++ Casks/crypto-org-chain-desktop-wallet.rb
@@ -1,6 +1,6 @@
 cask "crypto-org-chain-desktop-wallet" do
-  version "0.2.0"
-  sha256 "754a3c6cd0da1c587b610be3834d72c27629955a031b5b251bf054dbdf869fc4"
+  version "0.2.1"
+  sha256 "2267f1f76b74c6527972621898b2d6e51ec810de849992000ccf1d8849e0dd84"
 
   url "https://github.com/crypto-com/chain-desktop-wallet/releases/download/v#{version}/chain-desktop-wallet-#{version}.dmg"
   name "Crypto.org Chain Desktop Wallet (Beta)"
