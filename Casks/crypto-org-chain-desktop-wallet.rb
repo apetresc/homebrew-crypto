faa2f0ebf51dc382a1c077dbbfff5414ac1270e9
Adrian Petrescu <adrian@apetre.sc>
2021-07-09 20:37:46 -0400

    Upgrade crypto-org-chain-desktop-wallet v0.3.7

--- Casks/crypto-org-chain-desktop-wallet.rb
+++ Casks/crypto-org-chain-desktop-wallet.rb
@@ -1,6 +1,6 @@
 cask "crypto-org-chain-desktop-wallet" do
-  version "0.3.1"
-  sha256 "6c9352206cb29491862301c0b79c0c6e795eef1804bdc0f774fa30c25e950024"
+  version "0.3.7"
+  sha256 "61a70f14313f7234ebd970d53e6a758f1ad5a869ec16ffee985e56a0fb6d68fd"
 
   url "https://github.com/crypto-com/chain-desktop-wallet/releases/download/v#{version}/chain-desktop-wallet-#{version}.dmg"
   name "Crypto.org Chain Desktop Wallet (Beta)"
