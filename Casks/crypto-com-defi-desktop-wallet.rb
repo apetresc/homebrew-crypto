938d2f1d8dd73bd1b05c4351233fb612e1838542
Adrian Petrescu <adrian@apetre.sc>
2022-11-01 19:57:42 -0400

    Update crypto-com-defi-desktop-wallet from 1.3.1 to 1.3.3

--- Casks/crypto-com-defi-desktop-wallet.rb
+++ Casks/crypto-com-defi-desktop-wallet.rb
@@ -1,6 +1,6 @@
 cask "crypto-com-defi-desktop-wallet" do
-  version "1.3.1"
-  sha256 "337888b32ce620106e5acf0484d6de932150a95da179b5793387e3d1a26fb344"
+  version "1.3.3"
+  sha256 "9314751c98a39472e239452934b18b67e9daeddd8c020e9b8452ab53fa41ce7a"
 
   url "https://github.com/crypto-com/chain-desktop-wallet/releases/download/v#{version}/Crypto.com-DeFi-Desktop-Wallet-#{version}.dmg"
   name "Crypto.com DeFi Desktop Wallet"
